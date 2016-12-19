926d23a4694ab864164b82164a275a8c85461c42
Miztah <miztah@swgemu.com>
2016-12-19 18:45:39 -0500

    [added] Christmas stuff
    Change-Id: Idfd88bf4b0ca877cd96cc1ed9797818fbaaf3b73

---
+++ MMOCoreORB/bin/scripts/mobile/swgemu/christmas_jawa_redwhite.lua
@@ -0,0 +1,49 @@
+christmas_jawa_redwhite = Creature:new {
+	customName = "a Christmas Jawa",
+	objectName = "",
+	randomNameType = NAME_GENERIC,
+	randomNameTag = true,
+	socialGroup = "jawa",
+	faction = "jawa",
+	level = 6,
+	chanceHit = 0.25,
+	damageMin = 50,
+	damageMax = 55,
+	baseXp = 147,
+	baseHAM = 180,
+	baseHAMmax = 220,
+	armor = 0,
+	resists = {0,0,0,0,0,0,0,-1,-1},
+	meatType = "",
+	meatAmount = 0,
+	hideType = "",
+	hideAmount = 0,
+	boneType = "",
+	boneAmount = 0,
+	milk = 0,
+	tamingChance = 0,
+	ferocity = 0,
+	pvpBitmask = ATTACKABLE,
+	creatureBitmask = PACK + STALKER,
+	optionsBitmask = AIENABLED,
+	diet = HERBIVORE,
+
+	templates = {"object/swgemu/christmas/mobile/jawa_redwhite.iff"},
+	lootGroups = {
+		{
+			groups = {
+				{group = "junk", chance = 5000000},
+				{group = "jawa_common", chance = 1500000},
+				{group = "armor_all", chance = 800000},
+				{group = "tailor_components", chance = 1000000},
+				{group = "loot_kit_parts", chance = 1500000},
+				{group = "color_crystals", chance = 200000}
+			}
+		}
+	},
+	weapons = {"jawa_warlord_weapons"},
+	conversationTemplate = "",
+	attacks = merge(marksmannovice,brawlernovice)
+}
+
+CreatureTemplates:addCreatureTemplate(christmas_jawa_redwhite, "christmas_jawa_redwhite")
---
+++ MMOCoreORB/bin/scripts/mobile/swgemu/gorax_grinch.lua
@@ -0,0 +1,48 @@
+gorax_grinch = Creature:new {
+	customName = "The Grinch",
+	objectName = "",
+	socialGroup = "gorax",
+	faction = "",
+	level = 300,
+	chanceHit = 30,
+	damageMin = 1645,
+	damageMax = 3000,
+	baseXp = 28549,
+	baseHAM = 385000,
+	baseHAMmax = 471000,
+	armor = 3,
+	resists = {165,145,35,35,35,35,35,35,-1},
+	meatType = "",
+	meatAmount = 0,
+	hideType = "",
+	hideAmount = 0,
+	boneType = "",
+	boneAmount = 0,
+	milk = 0,
+	tamingChance = 0,
+	ferocity = 20,
+	pvpBitmask = AGGRESSIVE + ATTACKABLE + ENEMY,
+	creatureBitmask = PACK + KILLER,
+	optionsBitmask = AIENABLED,
+	diet = CARNIVORE,
+
+	templates = {"object/swgemu/christmas/mobile/gorax_grinch.iff"},
+	lootGroups = {
+		{
+			groups = {
+				{group = "gorax_common", chance = 6000000},
+				{group = "armor_attachments", chance = 2000000},
+				{group = "clothing_attachments", chance = 2000000}
+			},
+			lootChance = 7000000
+		}
+	},
+	weapons = {},
+	conversationTemplate = "",
+	attacks = {
+		{"creatureareaattack",""},
+		{"creatureareaknockdown",""}
+	}
+}
+
+CreatureTemplates:addCreatureTemplate(gorax_grinch, "gorax_grinch")
